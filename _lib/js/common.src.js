2b7d16db0cd5657624e41ed161c634171fe78a49
Johnny Xu <gaga1227@gmail.com>
2013-10-11 11:50:25 +1100

    m1: Cleaned up content template
    along with CSS, JS for more specific updates

--- _lib/js/common.src.js
+++ _lib/js/common.src.js
@@ -86,7 +86,7 @@ function init(){
 	insertFirstLastChild('#navItems, #sideNav, #sideNav ul, .itemListing');
 	
 	//interactions	
-	SelectNav = new initSelectNav();
+	//SelectNav = new initSelectNav();
 	
 	//template specific functions
 	if 		( $('body#home').length ) 			{ initHome(); }
